d56c662241f90202bbcec608b53465053d0427f0
Maxime Israel <5600516+MaximeIJ@users.noreply.github.com>
2022-10-21 16:21:09 -0400

    Updates

--- stories/Scene.stories.d.ts
+++
@@ -1,6 +0,0 @@
-import type { StoryObj } from '@storybook/react-vite';
-import { SceneProps } from '../../../../../../../src/util/types';
-declare const _default: StoryObj<SceneProps>;
-export default _default;
-export declare const Default: StoryObj<SceneProps>;
-export declare const Big: StoryObj<SceneProps>;
